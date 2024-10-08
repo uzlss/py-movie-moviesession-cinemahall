74d5931a5c17a4e7baf0fd0d348c83d7a8dbe5eb
Ihor Bachuk <igor.bachuk04@gmail.com>
2024-10-08 14:32:14 +0300

    change logic in delete_movie_session_by_id

--- services/movie_session.py
+++ services/movie_session.py
@@ -45,4 +45,4 @@ def update_movie_session(
 
 
 def delete_movie_session_by_id(session_id: int) -> MovieSession:
-    return MovieSession.objects.get(id=session_id).delete()
+    return get_movie_session_by_id(session_id).delete()
